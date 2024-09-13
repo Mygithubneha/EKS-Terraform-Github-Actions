9e5e71a1a486b3b958f2db86d061cc67d320f411
Neha Avasekar <109887502+Mygithubneha@users.noreply.github.com>
2024-09-13 10:00:03 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -7,7 +7,7 @@ module "eks" {
   source = "../module"
 
   env                   = var.env
-  cluster-name          = "${local.env}-${local.org}-${var.cluster-name}"
+  cluster_name          = "${local.env}-${local.org}-${var.cluster-name}"
   cidr-block            = var.vpc-cidr-block
   vpc-name              = "${local.env}-${local.org}-${var.vpc-name}"
   igw-name              = "${local.env}-${local.org}-${var.igw-name}"
