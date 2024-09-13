9d21c3864f4d508ecf2955168350688432fd4a02
Neha Avasekar <109887502+Mygithubneha@users.noreply.github.com>
2024-09-13 10:06:30 +0530

    Update gather.tf

--- module/gather.tf
+++ module/gather.tf
@@ -1,5 +1,5 @@
 data "tls_certificate" "eks-certificate" {
-  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  url = aws_eks_cluster.eks[0].identity[0].oidc[0].issuer
 }
 
 data "aws_iam_policy_document" "eks_oidc_assume_role_policy" {
